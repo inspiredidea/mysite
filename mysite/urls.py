494756750fc59df5b227930e69d01f91ad8b7f03
Imran Yousaf <inspiredidea@gmail.com>
2020-05-05 08:02:11 +0500

    Created date and time view

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
+    path('time/', current_datetime),
 ]
