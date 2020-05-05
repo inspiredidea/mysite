fcca11420126627769b36c2b3871d4bef228d814
Imran Yousaf <inspiredidea@gmail.com>
2020-05-05 22:49:15 +0500

    Changed template location in settings.py file.

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'mysite/template')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
